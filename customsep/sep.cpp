6b71f4514814e705083853bcd824146b83d0828d
David Godfrey <david.godfrey99@gmail.com>
2014-08-11 12:35:00 -0400

    Shortened code
    I am able to decompose the code to a simple buffer and an output stream class that wraps it.

--- customsep/sep.cpp
+++ customsep/sep.cpp
@@ -1,106 +1,45 @@
 #include <iostream>
-    
-namespace custom
-{
-    struct sep_impl
-    {
-        sep_impl(std::string const& separator);
-        std::string separator;
-    }; 
-    
-    sep_impl sep(std::string const& str)
-    {
-        return sep_impl(str);
-    }
-    
-    std::ostream& nosep(std::ostream& os);
-}
- 
-int separatorEnabled()
-                      { static int idx = std::ios_base::xalloc(); return idx; }
-int getSeparator()    { static int idx = std::ios_base::xalloc(); return idx; }
- 
-struct custom_separator : std::streambuf
+
+class space_delimited_output_buffer : public std::streambuf
 {
 public:
-    custom_separator(std::ostream& _stream) : stream(_stream)
+    space_delimited_output_buffer(std::streambuf* sbuf)
+        : m_sbuf(sbuf)
     { }
-    
-    int_type overflow(int_type c)
+ 
+    virtual int_type overflow(int_type c)
     {
-        return stream.rdbuf()->sputc(c);
+        return m_sbuf->sputc(c);
     }
-    
-    int sync()
+ 
+    virtual int sync()
     {
-        if (stream.iword(separatorEnabled()))
-        {
-            void*& p = stream.pword(getSeparator());
-            stream << *static_cast<std::string*>(p);
-            return 0;
-        }
-        return stream.rdbuf()->pubsync();
+        if (ok_to_write)
+            this->sputc(' ');
+        else
+            ok_to_write = true;
+ 
+        return internal_sync();
     }
 private:
-    std::ostream& stream;
+    std::streambuf* m_sbuf;
+    bool ok_to_write = false;
+ 
+    int internal_sync()
+    {
+        return m_sbuf->pubsync();
+    }
 };
  
-void cleanup(std::ios_base::event evt, std::ios_base& str, int idx)
+class space_delimited_output_stream
+    : public std::ostream
+    , private virtual space_delimited_output_buffer
 {
-    if (str.iword(separatorEnabled()) && evt == std::ios_base::erase_event)
+public:
+    space_delimited_output_stream(std::ostream& os)
+        : std::ostream(this)
+        , space_delimited_output_buffer(os.rdbuf())
     {
-        void*& p = str.pword(idx);
-        delete static_cast<std::string*>(p);
-        str.iword(separatorEnabled()) = false; 
+        this->tie(this); 
     }
-}
- 
-std::ostream& set_separator(std::ostream& os, const custom::sep_impl& manip)
-{
-    if (!os.bad())
-    {
-        os.pword(getSeparator()) = new std::string(manip.separator);
-        os.register_callback(cleanup, getSeparator());
-    }
-    
-    return os;
-}
- 
-std::ostream& operator<<(std::ostream& os, const custom::sep_impl& manip)
-{
-    std::ostream* p = os.tie();
-    if (p && !p->iword(separatorEnabled()))
-    {
-        set_separator(*p, manip);
-        p->iword(separatorEnabled()) = true;
-    }
-    
-    return os << std::unitbuf;
-}
- 
-namespace custom
-{
-    sep_impl::sep_impl(std::string const& _sep) : separator(_sep) { }
-    
-    std::ostream& nosep(std::ostream& os)
-    {
-        cleanup(std::ios_base::erase_event, *os.tie(), getSeparator());
-        os.tie(nullptr);
-        return os << std::nounitbuf;
-    }
-    
-    void install_separator(std::ostream& o1, std::ostream& o2)
-    {
-        static custom_separator csep(o2);
-        o1.rdbuf(&csep);
-        o1.tie(&o2);
-    }
-}
- 
-int main()
-{
-    std::ostream os(nullptr);
-    custom::install_separator(os, std::cout);
-    
-    os << custom::sep(", ") << 4 << 2 << custom::nosep;
-}
+};
